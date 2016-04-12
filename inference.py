59b9a6df03d1c73ca3f099361574b395497fe3d9
Ramon de Oliveira <mail@roliveira.net>
2016-04-12 01:17:36 -0300

    It's working :)

--- inference.py
+++ inference.py
@@ -45,13 +45,13 @@ b1 = tf.Variable(initial_b1, dtype=tf.float32, name='b1')
 W1 = tf.random_normal([hidden, out_dim], dtype=tf.float32, name='W1')
 
 # * is elementwise operator
-ff0 = tf.nn.relu(tf.matmul(X_batch, W0*stddev0 + mean0) + b0)
-ff1 = tf.matmul(ff0, W1*stddev1 + mean1) + b1
+ff0 = tf.nn.relu(tf.matmul(X_batch, tf.mul(W0, stddev0) + mean0) + b0)
+ff1 = tf.matmul(ff0, tf.mul(W1, stddev1) + mean1) + b1
 y_out = tf.nn.sigmoid(ff1)
 
-#cost = tf.nn.sigmoid_cross_entropy_with_logits(ff1, y_batch)
-cost = -y_batch*tf.clip_by_value(tf.log(y_out), -1e6, 1e6) - \
-       (1.0 - y_batch)*tf.clip_by_value(tf.log(1.0 - y_out), -1e6, 1e6) 
+cost = tf.nn.sigmoid_cross_entropy_with_logits(ff1, y_batch)
+#cost = -y_batch*tf.clip_by_value(tf.log(y_out), -1e6, 1e6) - \
+#       (1.0 - y_batch)*tf.clip_by_value(tf.log(1.0 - y_out), -1e6, 1e6) 
 #cost = y_out - y_out*y_batch + tf.log(1 + tf.exp(-y_out))
        
 #cost = y_batch*-tf.log(y_out) + (1.0 - y_batch)*-tf.log(1.0 - y_out)
@@ -64,27 +64,31 @@ KL = tf.reduce_sum(KL0) + tf.reduce_sum(KL1)
 loss = KL + cost
 
 ws = [mean0, stddev0, b0, mean1, stddev1, b1]
-train = tf.train.GradientDescentOptimizer(0.1).minimize(loss, var_list=ws)
+train = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
 
 init = tf.initialize_all_variables()
 
 sess = tf.Session()
 sess.run(init)
 
-for i in range(100):
-    if i % 10000 == 0:
-        o, oi, c, k, l = sess.run([y_out, ff1, cost, KL, loss], feed_dict={
-            X_batch: X_train[:1], y_batch: y_train[:1]})
+for i in range(1001):
+    if i % 100 == 0:
+        o, oi, c, k, l = sess.run([y_out, ff1, cost, KL, loss], 
+                                  feed_dict={X_batch: X_train[:1], y_batch: y_train[:1]})
         print('Step', i+1)
         print('output:')
         print(o, oi, y_train[0])
         print('cost:', c)
         print('KL:', k)
         print('loss:', l)
+        m0, s0 = sess.run([mean0, stddev0])
+        print('mean0 sum:', np.sum(m0) ,'stddev0 sum:', np.sum(s0))
     sess.run(train, feed_dict={X_batch: X_train[i:i+1], 
                                y_batch: y_train[i:i+1]})
-    m0, s0 = sess.run([mean0, stddev0])
-    print('mean0 sum:', np.sum(m0) ,'stddev0 sum:', np.sum(s0))
+    #grad = tf.gradients(loss, [mean0])[0]
+    #vg = sess.run(grad, feed_dict={X_batch: X_train[:1], y_batch: y_train[:1]})
+    #print('gradient:', len(vg), len(vg[0]))
+    #print('gradient:', vg)
 
 #sess = tf.InteractiveSession()
 sess.close()
