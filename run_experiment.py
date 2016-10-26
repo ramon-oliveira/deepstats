9598787b58ed8d97e7a5ddd50548e3a168c348e5
Ramon de Oliveira <ramono@motorola.com>
2016-10-25 23:44:22 -0200

    skip experiment already done

--- run_experiment.py
+++ run_experiment.py
@@ -52,24 +52,19 @@ def run_experiment(dataset, model, with_unknown):
 
     try:
         df = pd.read_csv(os.path.join(results_folder, filename))
-        print('Loaded file:', os.path.join(results_folder, filename))
+        print('Load file:', os.path.join(results_folder, filename))
     except:
         df = pd.DataFrame()
 
     for idx, (inside_labels, unknown_labels) in enumerate(labels):
         inside_labels.sort()
         unknown_labels.sort()
-        if 'inside_labels' in df.columns:
-            aux = df[df.inside_labels == str(inside_labels)]
-            aux = aux[aux.unknown_labels == str(unknown_labels)]
-            if len(aux) == 5:
-                print('Skipping', str(inside_labels), str(unknown_labels))
-            rep = 5 - len(aux)
-        else:
-            rep = 5
-            
-        for i in range(rep):
+
+        for i in range(5):
             experiment_name = '{}.{}'.format(idx+1, i+1)
+            if 'experiment_name' in df.columns:
+                if np.float64(experiment_name) in np.float64(df.experiment_name.values):
+                    continue
             out = anomaly(experiment_name, model, dataset,
                           inside_labels, unknown_labels,
                           with_unknown=with_unknown)
